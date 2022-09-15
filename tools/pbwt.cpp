86ad40d55a14f5988a94a7ad4fb5e94927f4b3cf
Zilong-Li <zimusen94@gmail.com>
2022-09-15 10:09:44 +0100

    add pbwt

--- tools/pbwt.cpp
+++ tools/pbwt.cpp
@@ -1,18 +1,20 @@
-#include "vcfpp.h"
+// -*- compile-command: "g++ pbwt.cpp -o pbwt -std=c++17 -g -O3 -Wall -lhts -lz -lm -lbz2 -llzma -lcurl && ./pbwt " -*-
 #include <fstream>
+#include <vcfpp.h>
 
 using namespace std;
 using namespace vcfpp;
 
 typedef uint32_t uint;
+typedef uint64_t uint2;
 
 /// build and dump pbwt FM-index to disk;
 void pbwt_index(const std::string& vcffile, const std::string& samples, const std::string& region)
 {
     BcfReader vcf(vcffile, samples, region);
     BcfRecord var(vcf.header);
-    vector<bool> gt;
-    vector<vector<bool>> x;
+    std::vector<bool> gt;
+    std::vector<std::vector<bool>> x;
     int nsnps = 0, nsamples = vcf.nsamples;
     while (vcf.getNextVariant(var))
     {
@@ -23,69 +25,65 @@ void pbwt_index(const std::string& vcffile, const std::string& samples, const st
         nsnps++;
     }
     // build pbwt index
-    uint N = nsnps, M = nsamples * 2;
-    ofstream ofpbwt(vcffile + ".pbwt", std::ios::binary);
-    ofstream ofauxu(vcffile + ".auxu", std::ios::binary);
-    ofstream ofauxv(vcffile + ".auxv", std::ios::binary);
+    int N = nsnps, M = nsamples * 2;
+    std::ofstream ofpbwt(vcffile + ".pbwt", std::ios::binary);
+    std::ofstream ofauxu(vcffile + ".auxu", std::ios::binary);
+    std::ofstream ofauxv(vcffile + ".auxv", std::ios::binary);
     ofpbwt.write(reinterpret_cast<char*>(&N), 4);
     ofpbwt.write(reinterpret_cast<char*>(&M), 4);
-    vector<uint> at(M), a(M), a0(M), a1(M), u(M + 1), v(M + 1);
+    std::vector<uint> at(M), a(M), a0(M), a1(M), u(M + 1), v(M + 1);
     uint i, j, u_, v_, a_;
     for (j = 0; j < N; j++)
     {
-        u_ = 0; v_ = 0;
+        u_ = 0;
+        v_ = 0;
         // copy a to at
-        for (i = 0; i < M; i++) { at[i] = a[i]; }
+        for (i = 0; i < M; i++)
+        {
+            at[i] = a[i];
+        }
         for (i = 0; i < M; i++)
         {
             a_ = j > 0 ? at[i] : i;
-            u[i] = u_; v[i] = v_;
+            u[i] = u_;
+            v[i] = v_;
             if (x[j][a_])
-            {
-                a1[v_] = a_;
-                v_++;
-            }
+                a1[v_++] = a_;
             else
-            {
-                a0[u_] = a_;
-                u_++;
-            }
+                a0[u_++] = a_;
         }
-        u[M] = u_; v[M] = M;
+        u[M] = u_;
+        v[M] = M;
         for (i = 0; i < M; i++)
         {
             v[i] += u_;
             if (i < u_)
-            {
                 a[i] = a0[i];
-            }
             else
-            {
                 a[i] = a1[i - u_];
-            }
         }
         if (!ofpbwt.write(reinterpret_cast<char*>(&a[0]), a.size() * 4))
-            throw runtime_error(strerror(errno));
+            throw std::runtime_error(strerror(errno));
         if (!ofauxu.write(reinterpret_cast<char*>(&u[0]), u.size() * 4))
-            throw runtime_error(strerror(errno));
+            throw std::runtime_error(strerror(errno));
         if (!ofauxv.write(reinterpret_cast<char*>(&v[0]), v.size() * 4))
-            throw runtime_error(strerror(errno));
+            throw std::runtime_error(strerror(errno));
     }
 }
 
-void pbwt_query(const std::string& vcffile, const std::vector<bool>& z)
+std::vector<int> pbwt_query(const std::string& vcffile, const std::vector<bool>& z, int s, int L, int Step)
 {
-    uint N, M, i, j;
-    ifstream ifpbwt(vcffile + ".pbwt", std::ios::binary);
-    ifstream ifauxu(vcffile + ".auxu", std::ios::binary);
-    ifstream ifauxv(vcffile + ".auxv", std::ios::binary);
+    int N, M, i, j;
+    std::ifstream ifpbwt(vcffile + ".pbwt", std::ios::binary);
     if (!ifpbwt.read(reinterpret_cast<char*>(&N), 4))
-        throw runtime_error(strerror(errno));
+        throw std::runtime_error(strerror(errno));
     if (!ifpbwt.read(reinterpret_cast<char*>(&M), 4))
-        throw runtime_error(strerror(errno));
-    vector<uint> t(N), v(M + 1), u(M + 1), a(M);
-    vector<uint> matches;
-    uint s = 1, Step = 32, L = 2;
+        throw std::runtime_error(strerror(errno));
+    assert(z.size() == N);
+    std::ifstream ifauxu(vcffile + ".auxu", std::ios::binary);
+    std::ifstream ifauxv(vcffile + ".auxv", std::ios::binary);
+    std::vector<int> t(N), v(M + 1), u(M + 1), a(M);
+    std::vector<int> matches;
     for (i = 0; i < N; i++)
     {
         ifauxu.read(reinterpret_cast<char*>(&u[0]), 4 * (M + 1));
@@ -103,7 +101,7 @@ void pbwt_query(const std::string& vcffile, const std::vector<bool>& z)
         }
         if (s < N && i == s)
         {
-            ifpbwt.seekg((s - 1) * M * 4 + 8, ios_base::beg);
+            ifpbwt.seekg((uint2)s * M * 4 + 8, std::ios_base::beg);
             ifpbwt.read(reinterpret_cast<char*>(&a[0]), 4 * M);
             if (t[s] == M)
             {
@@ -124,15 +122,92 @@ void pbwt_query(const std::string& vcffile, const std::vector<bool>& z)
                     matches.push_back(a[(t[s] - j) > 0 ? (t[s] - j) : 0]);
                 // L haps after z;
                 for (j = 0; j < L; j++)
-                    matches.push_back(a[(t[s] + j) > M - 1] ? (t[s] + j) : (M - 1));
+                    matches.push_back(a[(t[s] + j) < M - 1 ? (t[s] + j) : (M - 1)]);
             }
             s += Step;
         }
     }
+    return matches;
 }
+
 
 int main(int argc, char* argv[])
 {
+    std::vector<std::string> args(argv + 1, argv + argc);
+    if (argc <= 1 || args[0] == "-h" || args[0] == "-help")
+    {
+        std::cout << "Usage example:\n"
+                  << "     " + (std::string)argv[0] + " index -vcf-panel VCF1 -samples ^S1,S2 -region chr1:1-1000 \n"
+                  << "     " + (std::string)argv[0] + " query -vcf-panel VCF1 -vcf-query VCF2 -out OUT \n\n"
+                  << "Options:\n"
+                  << "     -vcf-panel    vcf/bcf file of reference panel\n"
+                  << "     -vcf-query    vcf/bcf file of query sequence\n"
+                  << "     -sequence     single query sequence eg. 1010010\n"
+                  << "     -samples      list of samples to be included or excluded\n"
+                  << "     -region       specific region be included\n"
+                  << "     -out          output file\n"
+                  << "     -s (=1)       start position of the first selection\n"
+                  << "     -L (=2)       number of neighour haplotypes to be selected\n"
+                  << "     -S (=8)       make a selection every S SNPs\n"
+                  << "     -help         show help message\n"
+                  << std::endl;
+        return 1;
+    }
+    std::string vcfpanel, vcfquery, samples = "-", region, outfile, sequence;
+    int s = 1, L = 2, Step = 8;
+    for (int i = 1; i < args.size(); i++)
+    {
+        if (args[i] == "-vcf-panel")
+            vcfpanel = args[++i];
+        if (args[i] == "-vcf-query")
+            vcfquery = args[++i];
+        if (args[i] == "-sequence")
+            sequence = args[++i];
+        if (args[i] == "-samples")
+            samples = args[++i];
+        if (args[i] == "-region")
+            region = args[++i];
+        if (args[i] == "-out")
+            outfile = args[++i];
+        if (args[i] == "-s")
+            s = std::stoi(args[++i]);
+        if (args[i] == "-L")
+            L = std::stoi(args[++i]);
+        if (args[i] == "-S")
+            Step = std::stoi(args[++i]);
+    }
+    if (args[0] == "index")
+    {
+        pbwt_index(vcfpanel, samples, region);
+    }
+    else if (args[0] == "query")
+    {
+        if (!sequence.empty())
+        {
+            std::vector<bool> z(sequence.size());
+            for (int i = 0; i < sequence.size(); i++)
+                z[i] = (sequence[i] == '1');
+            auto res = pbwt_query(vcfpanel, z, s, L, Step);
+            // print out results
+            for (auto& o : res)
+                std::cout << o << std::endl;
+        }
+        else if (!vcfquery.empty())
+        {
+            // todo
+            std::cout << "implementation missing for -vcf-query\n";
+        }
+        else
+        {
+            std::cout << "please use either -vcf-query or -sequence option for query\n";
+            return 1;
+        }
+    }
+    else
+    {
+        std::cout << "please run either index or query. use -h to see usage example\n";
+        return 1;
+    }
 
     return 0;
 }
