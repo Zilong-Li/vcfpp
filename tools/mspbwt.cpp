16b25bde6997b808a9e1f5db3954734bd22e778d
Zilong-Li <zimusen94@gmail.com>
2022-12-04 12:41:20 +0100

    complexity O(log(n)) of each grid

--- tools/mspbwt.cpp
+++ tools/mspbwt.cpp
@@ -2,20 +2,29 @@
 #include "../vcfpp.h"
 
 #include <algorithm>
-#include <bitset>
 #include <map>
 #include <numeric>
+#include <random>
 #include <set>
+#include <unordered_map>
 
 
 using namespace std;
 using namespace vcfpp;
 
-// const int B{sizeof(T) * 8};
-static const int B = 32;
+using IntMap = unordered_map<int, int>;
+using IntVecMap = unordered_map<int, std::vector<int>>;
+using WgSymbolMap = map<int, map<int, int>>;
 
-void mspbwt_encode(const std::string& vcffile, const std::string& samples, const std::string& region);
-std::map<int, std::vector<int>> build_W(const vector<int>& x, int cap = 0);
+void cout_WgSymbolMap(const WgSymbolMap& wg);
+vector<int> encodeZ(const vector<bool>& z, int G);
+vector<bool> randhapz(int M);
+int find_closest_symbol2zg(int zg, const std::set<int>& symbols);
+IntMap build_C(const vector<int>& x);
+IntMap save_Symbols(const vector<int>& x);
+WgSymbolMap save_W(const vector<int>& x);
+IntVecMap build_W(const vector<int>& x);
+void mspbwt(const std::string& vcffile, const std::string& samples, const std::string& region);
 
 int main(int argc, char* argv[])
 {
@@ -24,8 +33,6 @@ int main(int argc, char* argv[])
     if (argc <= 1 || args[0] == "-h" || args[0] == "-help")
     {
         std::cout << "Author: Zilong-Li (zilong.dk@gmail.com)\n"
-                  << "Description:\n"
-                  << "     create DS tag for diploid samples given GP or GT tag from input vcf file\n\n"
                   << "Usage example:\n"
                   << "     " + (std::string)argv[0] + " -i in.bcf \n"
                   << "     " + (std::string)argv[0] + " -i in.bcf -o out.bcf -s ^S1,S2 -r chr1:1-1000 \n"
@@ -53,33 +60,164 @@ int main(int argc, char* argv[])
 
     // ========= core calculation part ===========================================
 
-    mspbwt_encode(invcf, samples, region);
+    mspbwt(invcf, samples, region);
 
     return 0;
 }
 
-void mspbwt_encode(const std::string& vcffile, const std::string& samples, const std::string& region)
+vector<int> encodeZ(const vector<bool>& z, int G)
 {
+    vector<int> zg(G);
+    const int B = 32;
+    size_t m{0}, k{0}, M{z.size()};
+    for (m = 0; m < M; m++)
+    {
+        zg[k] = (zg[k] << 1) | (z[m] != 0);
+        if ((m + 1) % B == 0)
+            k++;
+    }
+    if (G == k + 1)
+    {
+        zg[k] <<= G * B - M; // padding 0s
+    }
+    else if (G == k)
+    {
+        cerr << "no need padding\n";
+    }
+    else
+    {
+        throw std::runtime_error("something wrong\n");
+    }
+    return zg;
+}
+
+void cout_WgSymbolMap(const WgSymbolMap& wg)
+{
+    auto print_key_value = [](const auto& key, const auto& value) { std::cout << "Index:[" << key << "] rank:[" << value << "]\n"; };
+    for (const auto& [s, v] : wg)
+    {
+        cout << "symbol:[" << s << "]\n";
+        for (const auto& [idx, i] : v)
+        {
+            print_key_value(idx, i);
+        }
+    }
+}
+
+vector<bool> randhapz(int M)
+{
+    std::random_device rd;  // Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> dist(0, 1);
+    vector<bool> z(M);
+    for (int i = 0; i < M; i++)
+    {
+        z[i] = dist(gen);
+    }
+    return z;
+}
+
+int find_closest_symbol2zg(int zg, const std::set<int>& symbols)
+{
+    // vec is ordered. so binary search
+    // auto lower = std::lower_bound(symbols.begin(), symbols.end(), zg);
+    auto lower = symbols.lower_bound(zg);
+    if (lower != symbols.end())
+        cerr << "no end\n";
+    if (lower != symbols.end())
+        return *lower;
+    else
+        return *symbols.rbegin();
+}
+
+IntMap save_Symbols(const vector<int>& x)
+{
+    set<int> s(x.begin(), x.end()); // convert to set which unique sorted
+    IntMap symbols;                 // key: symbol; value: index in set
+    int n{0};
+    for (const auto& si : s)
+        symbols[si] = n++;
+    return symbols;
+}
+
+IntMap build_C(const vector<int>& x)
+{
+    set<int> s(x.begin(), x.end()); // convert to set which unique sorted
+    IntMap C;
+    int c{0}, n{0}, cap{0};
+    for (const auto& si : s)
+    {
+        c = 0;
+        for (const auto& xi : x)
+        {
+            if (xi < si)
+                c++;
+        }
+        C[si] = c;
+        if (++n == cap && cap > 0)
+            break;
+    }
+
+    return C;
+}
+
+WgSymbolMap save_W(const vector<int>& x)
+{
+    set<int> s(x.begin(), x.end()); // convert to set which unique sorted
+    size_t i{0}, k{0};
+    WgSymbolMap W;
+    for (const auto& si : s)
+    {
+        k = 0;
+        for (i = 0; i < x.size(); i++)
+        {
+            if (x[i] == si)
+                W[si][i] = k++; // k: how many occurrences before i
+        }
+    }
+
+    return W;
+}
+
+IntVecMap build_W(const vector<int>& x)
+{
+    set<int> s(x.begin(), x.end()); // convert to set which unique sorted
+    auto C = build_C(x);
+    int c{0}, i{0}, n{0}, cap{0};
+    IntVecMap W;
+    for (auto& si : s)
+    {
+        W[si] = vector<int>(x.size());
+        c = 0;
+        for (i = 0; i < x.size(); i++)
+        {
+            if (x[i] == si)
+                c++;
+            W[si][i] = c + C[si];
+        }
+        if (++n == cap && cap > 0)
+            break;
+    }
+
+    return W;
+}
+
+void mspbwt(const std::string& vcffile, const std::string& samples, const std::string& region)
+{
+    const int B = 32;
     BcfReader vcf(vcffile, samples, region);
     BcfRecord var(vcf.header);
-    vector<char> gt;
     uint64_t N{0}, M{0}, G{0}, k{0}, m{0}, i{0}; // N haplotypes, M SNPs, G Grids, k Current grid, m Current SNP
     N = vcf.nsamples * 2;
-    while (vcf.getNextVariant(var))
-    {
-        if (!var.isSNP())
-            continue; // skip other type of variants
-        M++;
-    }
+    M = vcf.get_region_records(region);
     G = (M + B - 1) / B;
-    cerr << N << "\t" << M << "\t" << G << endl;
+    cerr << "Haps(N):" << N << "\tSNPs(M):" << M << "\tGrids(G):" << G << "\tInt(B):" << B << endl;
     vector<vector<int>> X; // Grids x Haps
     X.resize(G, vector<int>(N));
     vcf.setRegion(region);
+    vector<bool> gt;
     while (vcf.getNextVariant(var))
     {
-        if (!var.isSNP())
-            continue; // skip other type of variants
         var.getGenotypes(gt);
         // update current grids
         for (i = 0; i < N; i++)
@@ -88,7 +226,6 @@ void mspbwt_encode(const std::string& vcffile, const std::string& samples, const
         if (m % B == 0)
             k++; // update next grid
     }
-    cerr << m << "\t" << k << "\t" << B << endl;
     if (G == k + 1)
     {
         int pad = G * B - M;
@@ -103,9 +240,11 @@ void mspbwt_encode(const std::string& vcffile, const std::string& samples, const
     {
         throw std::runtime_error("something wrong\n");
     }
+
     vector<vector<int>> A; // (Grids+1) x Haps
     A.resize(G + 1, vector<int>(N));
-    // vector<std::map<int, std::vector<int>>> W(G);
+    vector<WgSymbolMap> W(G);
+    vector<IntMap> C(G);
     vector<int> a0(N), y0(N);
     for (k = 0; k < G; k++)
     {
@@ -116,72 +255,40 @@ void mspbwt_encode(const std::string& vcffile, const std::string& samples, const
         }
         for (i = 0; i < N; i++)
             y0[i] = X[k][a0[i]];
-        auto Wg = build_W(y0); // TODO: don't cap symbols when building indices
-        // W[k] = Wg;          // save current W
+        auto Wg = build_W(y0); // here Wg is S x N
         for (i = 0; i < N; i++)
         {
             A[k + 1][Wg[y0[i]][i] - 1] = a0[i];
         }
-        a0 = A[k + 1]; // next run
-
-        // for (const auto& [s, occ] : Wg)
-        // {
-        //     cout << s << ": ";
-        //     for (auto& oi : occ)
-        //     {
-        //         cout << oi << "\t";
-        //     }
-        //     cout << endl;
-        // }
-        // for (k = 0; k < N; k++)
-        //     cout << X[k][i] << "\t";
-        // cout << endl;
+        // next run
+        a0 = A[k + 1];
+        // save current W, which is of N+2*S length only. NB first two elements are rank, C[si]
+        W[k] = save_W(y0);
+        C[k] = build_C(y0);
     }
 
-    for (auto & ai : A) {
-        for (auto & v : ai) {
-            cout << v << "\t";
-        }
-        cout << endl;
-    }
-
-}
-
-
-std::map<int, std::vector<int>> build_W(const vector<int>& x, int cap)
-{
-    std::set<int> s(x.begin(), x.end()); // convert to set which unique sorted
-    std::map<int, int> C;
-    int c{0}, i{0}, n{0};
-    for (auto& si : s)
+    // finially insert zg back to A at each grid
+    auto z = randhapz(M);
+    auto zg = encodeZ(z, G);
+    vector<int> ta(G);
+    k = 0;
+    // search for the closest symbol to zg[k] in W[k].keys() if not exists
+    auto wz = W[k].lower_bound(zg[k]) != W[k].end() ? *W[k].lower_bound(zg[k]) : *W[k].crbegin();
+    for (const auto& si : W[k])
     {
-        c = 0;
-        for (auto& xi : x)
-        {
-            if (xi < si)
-                c++;
-        }
-        C[si] = c;
-        if (++n == cap && cap > 0)
-            break;
+        cerr << si.first << ",";
     }
-    std::map<int, std::vector<int>> W;
-    n = 0;
-    for (auto& si : s)
+    cerr << "\nzg:" << zg[k] << "\t" << wz.first << endl;
+    cout_WgSymbolMap(W[k]);
+    // for k=0, start with random occurrence of the symbol. and put zg at that position
+    ta[k] = wz.second.cbegin()->first;
+    cerr << "k:" << k << "\tclosest symbol to zg:" << wz.first << "\torder in a[k]:" << ta[k] << endl;
+    // for k > 0
+    for (k = 1; k < G; k++)
     {
-        W[si] = vector<int>(x.size());
-        c = 0;
-        for (i = 0; i < x.size(); i++)
-        {
-            if (x[i] == si)
-                c++;
-            W[si][i] = c + C[si];
-        }
-        if (++n == cap && cap > 0)
-            break;
+        auto wz = W[k].lower_bound(zg[k]) != W[k].end() ? *W[k].lower_bound(zg[k]) : *W[k].crbegin();
+        auto wi = wz.second.lower_bound(ta[k-1]) != wz.second.end() ? *wz.second.lower_bound(ta[k-1]) : *wz.second.crbegin();
+        ta[k] = wi.second + C[k][wz.first];
+        cerr << "k:" << k << "\tclosest symbol to zg:" << wz.first << "\torder in a[k]:" << ta[k] << endl;
     }
-    return W;
-    // for (auto & i : s)
-    //     cout << bitset<32>(i) << "\t";
-    // cout << endl;
 }
